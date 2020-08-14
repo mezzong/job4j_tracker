474f108b0802629c3ad19515c48a8ddd035468c7
Stas <bazhoker@gmail.com>
2020-08-14 19:32:14 +0300

    1. Чтение кода и базовые ошибки [#332974]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -2,11 +2,11 @@ package ru.job4j.tracker;
 
 public class Tracker {
     private final Item[] items = new Item[100];
-    private int ids = 1;
+    private int id = 1;
     private int size = 0;
 
     public Item add(Item item) {
-        item.setId(ids++);
+        item.setId(id++);
         items[size++] = item;
         return item;
     }
