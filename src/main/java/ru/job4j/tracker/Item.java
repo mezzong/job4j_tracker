e73878be0ad5fad4edb8f4630e79d10def8d812b
Stas <bazhoker@gmail.com>
2020-08-19 15:52:05 +0300

    7. toString [#332994]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -32,4 +32,12 @@ public class Item {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,10 @@
+package ru.job4j.tracker;
+
+public class StartUI {
+
+    public static void main(String[] args) {
+        Item item = new Item();
+        item.setName("Task");
+        System.out.println(item);
+    }
+}
