563f326a8178fbd43a9e3ce79760f36b8171a1b3
Stas <bazhoker@gmail.com>
2020-09-06 21:11:15 +0300

    6. Метод замены заявки. Tracker.replace [#333002]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -14,15 +14,8 @@ public class Tracker {
     }
 
     public Item findById(int id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
-                break;
-            }
-        }
-        return rsl;
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
     }
 
     public Item[] findAll() {
@@ -40,5 +33,27 @@ public class Tracker {
             }
         }
         return Arrays.copyOf(rsl, rslSize);
+    }
+
+    public boolean replace(int id, Item item) {
+        boolean result = false;
+        int indexItem = indexOf(id);
+        if (items[indexItem].getId() == id) {
+            item.setId(id);
+            items[indexItem] = item;
+            result = true;
+        }
+        return result;
+    }
+
+    private int indexOf(int id) {
+        int rsl = -1;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getId() == id) {
+                rsl = index;
+                break;
+            }
+        }
+        return rsl;
     }
 }
