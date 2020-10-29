4a1a7cf2caafb2bd4b0e9169d4b282259bded664
Stas <bazhoker@gmail.com>
2020-10-29 19:02:19 +0300

    2. Список адресов [#332754]

---
+++ src/main/java/ru/job4j/stream/Profile.java
@@ -0,0 +1,21 @@
+package ru.job4j.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Profile {
+    private Address address;
+
+    public Profile(Address address) {
+        this.address = address;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public List<Address> collect(List<Profile> profiles) {
+        return profiles.stream().map(x -> x.address).collect(Collectors.toList());
+    }
+}
