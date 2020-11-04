2216a8318188e9f1eb49b3e6bf9ed17a9c8bcb88
Stas <bazhoker@gmail.com>
2020-11-04 22:46:50 +0300

    2. Local-Variable Type Inference [#332765]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -19,7 +19,7 @@ public class PhoneDictionary {
                 .or(predicateSurname)
                 .or(predicatePhone);
         ArrayList<Person> result = new ArrayList<>();
-        for (Person person : persons) {
+        for (var person : persons) {
             if (combine.test(person)) {
                 result.add(person);
             }
--- src/test/java/ru/job4j/search/PriorityQueueTest.java
+++ src/test/java/ru/job4j/search/PriorityQueueTest.java
@@ -12,7 +12,7 @@ import static org.junit.Assert.assertThat;
 public class PriorityQueueTest {
     @Test
     public void whenHigherPriority() {
-        PriorityQueue queue = new PriorityQueue();
+        var queue = new PriorityQueue();
         queue.put(new Task("low", 5));
         queue.put(new Task("low", 5));
         queue.put(new Task("low", 5));
@@ -22,7 +22,7 @@ public class PriorityQueueTest {
         queue.put(new Task("middle", 3));
         queue.put(new Task("middle", 3));
         queue.put(new Task("middle", 3));
-        Task result = queue.take();
+        var result = queue.take();
         assertThat(result.getDesc(), is("urgent"));
     }
 
