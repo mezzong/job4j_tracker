da9d402ae3df93762297f5088891fb3e874089fb
Stas <bazhoker@gmail.com>
2020-11-04 17:29:23 +0300

    4. Преобразование List в Map. [#332756]

--- src/main/java/ru/job4j/stream/ListToMap.java
+++ src/main/java/ru/job4j/stream/ListToMap.java
@@ -5,16 +5,9 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class ListToMap {
-  public static void main(String[] args) {
-      List<Student> studentList = List.of(new Student(1, "Smith"),
-              new Student(2, "Johnson"),
-              new Student(3, "Brown"),
-              new Student(1, "Garcia"),
-              new Student(1, "Smith")
-      );
-      Map<String, Student> studentMap = studentList.stream()
+  public static Map<String, Student> listToMap(List<Student> studentList) {
+      return studentList.stream()
               .distinct()
               .collect(Collectors.toMap(Student::getSurname, e -> e));
-      System.out.println(studentMap);
   }
 }
---
+++ src/test/java/ru/job4j/stream/ListToMapTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.stream;
+
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ListToMapTest{
+
+
+
+    @Test
+    public void whenCollectAddresses() {
+        List<Student> studentList = List.of(new Student(1, "Smith"),
+                new Student(2, "Johnson"),
+                new Student(3, "Brown"),
+                new Student(1, "Garcia"),
+                new Student(1, "Smith")
+        );
+        Map<String, Student> rsl = ListToMap.listToMap(studentList);
+        Map<String, Student> expected = new HashMap<>();
+        expected.put("Brown", new Student(3, "Brown"));
+        expected.put("Johnson", new Student(2, "Johnson"));
+        expected.put("Smith", new Student(1, "Smith"));
+        expected.put("Garcia", new Student(1, "Garcia"));
+
+        assertThat(rsl, is(expected));
+    }
+}
