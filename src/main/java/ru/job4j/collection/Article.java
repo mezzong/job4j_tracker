434b22664139dce7d0fec84a1017897d3c8f0f78
Stas <bazhoker@gmail.com>
2020-11-26 13:52:23 +0300

    1. Экзамен. Коллекции. [#332897]

--- src/main/java/ru/job4j/collection/Article.java
+++ src/main/java/ru/job4j/collection/Article.java
@@ -1,13 +1,14 @@
 package ru.job4j.collection;
 
+import java.util.HashSet;
+import java.util.List;
+
 public class Article {
     public static boolean generateBy(String origin, String line) {
-        String[] arr = line.split(" ");
-        for (String s : arr) {
-            if (!origin.contains(s)) {
-                return false;
-            }
-        }
-        return true;
+        HashSet<String> originSet = new HashSet<>(
+                List.of(origin.replaceAll("\\pP", "").split(" ")));
+        HashSet<String> lineSet = new HashSet<>(
+                List.of(line.replaceAll("\\pP", "").split(" ")));
+        return originSet.containsAll(lineSet);
     }
 }
